e04e86d52652daf4495564d6417e184ce4904929
Michelle Ferris <jmichellelim@yahoo.com>
2022-02-22 12:05:06 -0500

    getting error

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,19 +1,44 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class User {
+
+    @NotBlank(message = "Username is required")
+    @Size(min=5, max=15, message="Username must be between 5 and 15 characters")
     private String username;
+
+    @Email(message = "Please enter a valid email")
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Min(value=6, message="Password must be at least 6 characters")
     private String password;
+
+    @NotBlank(message = "Verify is required")
+    private String verify;
 
     public User() {
 
     }
 
-    public User(String username, String email, String password) {
+    public User(String username, String email, String password, String verify) {
         this();
         this.username = username;
         this.email = email;
         this.password = password;
+        this.verify=verify;
+    }
+
+    public String getVerify() {
+        return verify;
+    }
+
+    public void setVerify(String verify) {
+        this.verify = verify;
     }
 
     public String getUsername() {
